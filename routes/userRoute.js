27a48d437c6f3d9c9548aef2f4b47bd8c0891dce
RoyalAdvisor <johnsonepc@gmail.com>
2022-03-24 11:48:20 +0200

    user route updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -55,7 +55,8 @@ router.put("/:id", [verifyAcc, retrieveUser], async (req, res) => {
   if (req.body.username != null) res.user.username = req.body.username;
   if (req.body.email != null) res.user.email = req.body.email;
   if (req.body.password != null) {
-    req.user.password = bcrypt.hash(req.body.password, 8);
+    const newPassword = bcrypt.hashSync(req.body.password, 8);
+    req.user.password = newPassword;
   }
   if (req.body.profileImage != null)
     res.user.profileImage = req.body.profileImage;
