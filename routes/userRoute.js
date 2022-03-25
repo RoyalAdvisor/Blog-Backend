22764e3170212084e4760ba01aa9bc1c780c2d8d
RoyalAdvisor <johnsonepc@gmail.com>
2022-03-25 14:32:54 +0200

    added contact route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,7 @@ const user = require("../models/userModel");
 const verify = require("../middleware/verify");
 const { retrieveUser } = require("../middleware/retriever.js");
 const verifyAcc = require("../middleware/authJWT");
+const nodemailer = require("nodemailer");
 
 //Register
 
@@ -20,6 +21,32 @@ router.post("/signup", verify, async (req, res) => {
       profile: req.body.profile,
     });
     await newUser.save();
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.email,
+        pass: process.env.pass,
+      },
+    });
+    let mailOptions = {
+      from: process.env.email,
+      to: req.body.email,
+      subject: "Welcome to The Mental Mind!",
+      html: `
+        <h2>Thank you for signing up with us ${req.body.username}.</h2>
+        <h4>We're really glad that you chose to join out family and hope that you enjoy blogging with us!</h4>
+
+        <h4>Click the link below to go directly to the sign up page.</h4>
+        <a href="https://thementalmind-c60dd.web.app/signin">Click here!</a>
+      `,
+    };
+    transporter.sendMail(mailOptions, function (err, success) {
+      if (error) {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(200).send({ message: "Email was sent successfully." });
+      }
+    });
     res.status(200).send({ message: "Successfully created new user" });
   } catch (error) {
     res.status(500).send({ message: "Error creating new user" });
@@ -60,6 +87,32 @@ router.put("/:id", [verifyAcc, retrieveUser], async (req, res) => {
     res.user.password = bcrypt.hashSync(req.body.password, 8);
   try {
     const updateUser = await res.user.save();
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.email,
+        pass: process.env.pass,
+      },
+    });
+    let mailOptions = {
+      from: process.env.email,
+      to: res.user.email,
+      subject: "Your profile has been updated.",
+      html: `
+        <h2>Hello, ${res.user.username}.</h2>
+        <h4>We've noticed that you have changed your profile.</h4>
+
+        <h4>Click the link below to go directly to the sign up page.</h4>
+        <a href="https://thementalmind-c60dd.web.app/signin">Click here!</a>
+      `,
+    };
+    transporter.sendMail(mailOptions, function (err, success) {
+      if (error) {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(200).send({ message: "Email was sent successfully." });
+      }
+    });
     res.status(200).send(updateUser);
   } catch (error) {
     res.status(400).send({ message: error.message });
@@ -71,6 +124,29 @@ router.put("/:id", [verifyAcc, retrieveUser], async (req, res) => {
 router.delete("/:id", [verifyAcc, retrieveUser], async (req, res) => {
   try {
     await res.user.remove();
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.email,
+        pass: process.env.pass,
+      },
+    });
+    let mailOptions = {
+      from: process.env.email,
+      to: res.user.email,
+      subject: "Your account has been deleted.",
+      html: `
+        <h2>We really hate to see you go, ${res.user.username}.</h2>
+        <h4>However, we know all good things eventually come to an end. We wish you prosperity moving forward. Thank you for blogging with us!</h4>
+      `,
+    };
+    transporter.sendMail(mailOptions, function (err, success) {
+      if (error) {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(200).send({ message: "Email was sent successfully." });
+      }
+    });
     res.status(200).send({ message: "User deleted successfully." });
   } catch (error) {
     res.status(500).send({ message: error.message });
