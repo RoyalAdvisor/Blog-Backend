8173c70f3eee348e321c10d537ca90cc07dba47d
RoyalAdvisor <johnsonepc@gmail.com>
2022-03-25 13:56:09 +0200

    added contact route

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
 const nodemailer = require("nodemailer");
 
-router.post("/", (req, res) => {
-  const { email, name, message } = req.body;
-  const transporter = nodemailer.createTransport({
+router.post("/", async (req, res) => {
+  let { email, name, message } = req.body;
+  let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: process.env.email,
       pass: process.env.pass,
     },
   });
-  const mailOptions = {
+
+  let mailOptions = {
     from: process.env.email,
     to: process.env.email,
     subject: `${name} has sent you a message!`,
@@ -19,14 +20,17 @@ router.post("/", (req, res) => {
         Don't forget to contact ${name} back at the following email address ${email}.
       `,
   };
-
-  transporter.sendMail(mailOptions, function (error, data) {
-    if (error) {
-      res.status(400).send({ message: "Email could not be sent." });
-    } else {
-      res.status(200).send({ message: "Email sent successfully." });
-    }
-  });
+  try {
+    transporter.sendMail(mailOptions, function (error, data) {
+      if (error) {
+        res.status(400).send({ message: "Email could not be sent." });
+      } else {
+        res.status(200).send({ message: "Email sent successfully." });
+      }
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 module.exports = router;
